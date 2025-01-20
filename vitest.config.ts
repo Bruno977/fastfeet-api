dbf0eb2819c6f1f55b94506ef5a1c0b7577c2e29
Bruno Rodrigues <bruno.silveira@hsl.org.br>
2025-01-20 20:34:49 -0300

    initial commit

---
+++ vitest.config.ts
@@ -0,0 +1,18 @@
+import swc from 'unplugin-swc';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    alias: {
+      '@src': './src',
+    },
+    root: './',
+  },
+  resolve: {
+    alias: {
+      '@src': './src',
+    },
+  },
+  plugins: [swc.vite()],
+});
