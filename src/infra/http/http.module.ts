acd898ace576995a5c236da0c78ad0a0cda9151b
Bruno Rodrigues <bruno_151997@hotmail.com>
2025-02-19 11:35:10 -0300

    chore: refactor folders

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,7 +1,7 @@
 import { DatabaseModule } from './../database/database.module';
 import { Module } from '@nestjs/common';
-import { CreateAccountController } from './controllers/create-account';
-import { AuthenticateController } from './controllers/authenticate';
+import { CreateAccountController } from './controllers/delivery-man/create-account';
+import { AuthenticateController } from './controllers/delivery-man/authenticate';
 import { RegisterDeliveryManUseCase } from 'src/domain/deliveries/application/use-cases/delivery-man/register-delivery-man';
 import { AuthenticateDeliveryManUseCase } from 'src/domain/deliveries/application/use-cases/delivery-man/authenticate-delivery-man';
 import { CryptographyModule } from '../cryptography/cryptography.module';
