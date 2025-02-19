253d463c6d503a2ad39ee6215f6acfafdf3c7415
Bruno Rodrigues <bruno_151997@hotmail.com>
2025-02-19 11:25:45 -0300

    fix: config vitest

--- vitest.config.e2e.ts
+++ vitest.config.e2e.ts
@@ -1,19 +1,17 @@
+import { resolve } from 'path';
 import swc from 'unplugin-swc';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    globals: true,
     include: ['**/*.e2e-spec.ts'],
-    alias: {
-      '@src': './src',
-    },
+    globals: true,
     root: './',
     setupFiles: ['./test/setup-e2e.ts'],
   },
   resolve: {
     alias: {
-      '@src': './src',
+      src: resolve(__dirname, './src'),
     },
   },
   plugins: [swc.vite()],
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,17 +1,15 @@
+import { resolve } from 'path';
 import swc from 'unplugin-swc';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
     globals: true,
-    alias: {
-      '@src': './src',
-    },
     root: './',
   },
   resolve: {
     alias: {
-      '@src': './src',
+      src: resolve(__dirname, './src'),
     },
   },
   plugins: [swc.vite()],
